8f56a65ab6883c71f58bea4e90c331da5a9ef556
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-01-23 23:09:02 +0000

    Test Base has been added

--- src/test/java/com/totdomvc/tests/TestBase.java
+++ src/test/java/com/totdomvc/tests/TestBase.java
@@ -1,30 +1,40 @@
 package com.totdomvc.tests;
 
 import com.totdomvc.utilities.Configurations_Reader;
-import com.totdomvc.utilities.Driver;
+import static com.totdomvc.utilities.Driver.*;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Actions actions;
 
 
-
-    WebDriver driver;
-    WebDriverWait wait;
-    Actions actions;
     @BeforeMethod
     public void setUp(){
-        driver = Driver.getDriver();
+        driver = getDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         String url = Configurations_Reader.getKey("url");
         actions = new Actions(driver);
         wait = new WebDriverWait(driver,10);
         driver.get(url);
-
     }
+
+
+
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        closeDriver();
+    }
+
 }
