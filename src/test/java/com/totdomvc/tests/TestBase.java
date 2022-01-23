2513b65da46c56a0334e00b9f0cc4f20135e9bf8
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-01-23 19:34:55 +0000

    Test Base has been added

---
+++ src/test/java/com/totdomvc/tests/TestBase.java
@@ -0,0 +1,30 @@
+package com.totdomvc.tests;
+
+import com.totdomvc.utilities.Configurations_Reader;
+import com.totdomvc.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+
+
+    WebDriver driver;
+    WebDriverWait wait;
+    Actions actions;
+    @BeforeMethod
+    public void setUp(){
+        driver = Driver.getDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        String url = Configurations_Reader.getKey("url");
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver,10);
+        driver.get(url);
+
+    }
+}
